13c33e737d344d2cc027fb7ddf4044916f390a7e
Agustin Granes <AgustinGranes@users.noreply.github.com>
2026-07-19 03:28:12 -0300

    style: increase vertical padding on unsplit medium widget to 30px

--- tmp/widget.js
+++ tmp/widget.js
@@ -28,7 +28,7 @@ try {
     if (isSplit) {
       widget.setPadding(14, 14, 14, 14); // Menos padding para no asfixiar el contenido dividido
     } else {
-      widget.setPadding(25, 18, 25, 18); // Más padding arriba/abajo, menos a los costados
+      widget.setPadding(30, 18, 30, 18); // Más padding arriba/abajo, menos a los costados
     }
   }
   
